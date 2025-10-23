f69e25c90a7c489ea51a5a394659b449c2539e94
James <jamesdev4you@gmail.com>
2025-10-23 16:16:38 -0400

    lol

---
+++ src/PageComponents/AboutMe/aboutme.js
@@ -0,0 +1,21 @@
+import React from "react";
+import NavBar from "../NavBar/navbar.js";
+import Footer from "../Footer/footer.js";
+import { Box } from "@mui/material";
+
+const aboutme = () => {
+  return (
+    <div>
+      <NavBar />
+      <Box
+        sx={{
+          width: "100%",
+          height: "1px",
+          backgroundColor: "secondary.main",
+        }}
+      />
+      <Footer />
+    </div>
+  );
+};
+export default aboutme;
